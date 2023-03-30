6499de00866f72b1c3db694bd72570ef06e9eab9
Raghav Agarwal <agarwalraghav892@gmail.com>
2023-03-30 20:10:44 +0530

    Changed Socket URL

--- client/src/Services/Socket.js
+++ client/src/Services/Socket.js
@@ -1,4 +1,4 @@
 import io from "socket.io-client";
-const SOCKET_URL = "http://localhost:3601";
+const SOCKET_URL = "https://chat-app-raghav-03.vercel.app/";
 
 export const socket = io(SOCKET_URL);
